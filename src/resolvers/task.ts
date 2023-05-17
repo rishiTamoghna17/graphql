a56bfaaf21de5eb0b49611feee59e7e76cad05e8
rishiTamoghna17 <tamoghna171099@gmail.com>
2023-05-17 18:17:32 +0530

    "node module"

--- src/resolvers/task.ts
+++ src/resolvers/task.ts
@@ -1,5 +1,13 @@
 import { PrismaClient, Task } from "@prisma/client";
-import { ObjectType, Field, Resolver, Arg, Mutation, Query, Int } from "type-graphql";
+import {
+  ObjectType,
+  Field,
+  Resolver,
+  Arg,
+  Mutation,
+  Query,
+  Int,
+} from "type-graphql";
 
 @ObjectType()
 class TaskType {
@@ -34,11 +42,30 @@ export class TaskResolver {
   }
 
   @Mutation(() => TaskType)
-  async createTask(@Arg('title', { nullable: true }) title?: string): Promise<Task> {
-    const task = await this.prisma.task.create({ data: { title } });
+  async createTask(
+    @Arg("title", { nullable: true }) title?: string,
+    @Arg("isComplete") isComplete?: boolean
+  ): Promise<Task> {
+    const task = await this.prisma.task.create({ data: { title,isComplete } });
     return task;
   }
+
+  @Mutation(() => TaskType)
+  async updateTask(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", { nullable: true }) title?: string,
+    @Arg("isComplete", { nullable: true }) isComplete?: boolean
+  ): Promise<Task | null> {
+    const updatedTask = await this.prisma.task.update({
+      where: { id },
+      data: { title, isComplete },
+    });
+    return updatedTask;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteTask(@Arg("id", () => Int) id: number): Promise<boolean> {
+    await this.prisma.task.delete({ where: { id } });
+    return true;
+  }
 }
-
-
-
